9ac39bb3c9caab4bdb333466ed85207626141908
chejianchao <820992376@qq.com>
2018-07-20 20:48:31 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,11 +1,13 @@
 class Backer
-  attr_reader :name
+  attr_reader :name, :back_projects
   def initialize(name)
     @name = name
     @back_projects = []
   end
   
-  def back_projects(project)
+  def back_project(project)
     @back_projects << project
   end
+  
+  
 end
